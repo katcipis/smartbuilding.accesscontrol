97155536650c23dc3fbfccbc7814966498b6ece0
Tiago César Katcipis <tiagokatcipis@gmail.com>
2011-10-04 23:21:26 -0300

    exemplo java completo

--- ice_example/java/compile.sh
+++ ice_example/java/compile.sh
@@ -1,2 +1,5 @@
 mkdir -p classes
 javac -d classes -classpath classes:/opt/Ice-3.4.2/lib/Ice.jar Server.java PrinterI.java generated/Demo/*.java
+javac -d classes -classpath classes:/opt/Ice-3.4.2/lib/Ice.jar Client.java PrinterI.java generated/Demo/*.java
+
+export CLASSPATH=$CLASSPATH:./classes:/opt/Ice-3.4.2/lib/Ice.jar
