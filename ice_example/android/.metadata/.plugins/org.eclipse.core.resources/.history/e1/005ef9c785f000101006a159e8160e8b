c7ce98d5d0b74d54a6b13f909244afedf9f6ee92
Tiago César Katcipis <tiagokatcipis@gmail.com>
2011-10-06 23:36:17 -0300

    adding server with right manifest file

---
+++ ice_example/android/.metadata/.plugins/org.eclipse.core.resources/.history/e1/005ef9c785f000101006a159e8160e8b
@@ -0,0 +1,13 @@
+package example.server;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ServerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
--- ice_example/android/server/src/example/server/ServerActivity.java
+++ ice_example/android/server/src/example/server/ServerActivity.java
@@ -9,5 +9,30 @@ public class ServerActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Ice.Communicator ic = null;
+        try {
+            ic = Ice.Util.initialize();
+            Ice.ObjectAdapter adapter =
+                ic.createObjectAdapterWithEndpoints("SimplePrinterAdapter", "default -p 10000");
+            Ice.Object object = new PrinterI();
+            adapter.add(object, ic.stringToIdentity("SimplePrinter"));
+            adapter.activate();
+            ic.waitForShutdown();
+        } catch (Ice.LocalException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        if (ic != null) {
+            // Clean up
+            //
+            try {
+                ic.destroy();
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+        }
+
     }
 }
