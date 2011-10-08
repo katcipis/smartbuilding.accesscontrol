b10d41676c7e24dd39749f4f9c7baa7ff725f522
Tiago César Katcipis <tiagokatcipis@gmail.com>
2011-10-08 13:29:54 -0300

    adding set env script

--- ice_example/android/client/src/client/example/ClientActivity.java
+++ ice_example/android/client/src/client/example/ClientActivity.java
@@ -2,12 +2,35 @@ package client.example;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ClientActivity extends Activity {
+	
+
+	// Create an anonymous implementation of OnClickListener
+	private OnClickListener printButtonListener = new OnClickListener() {
+	    public void onClick(View v) {
+	    	// Capture our edit text from layout
+	        EditText log = (EditText) findViewById(R.id.log);
+	        log.append("Lets call the printer class !!!\n");
+	    }
+	};
+
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.main);
+        
+        // Capture our button from layout
+        Button button = (Button)findViewById(R.id.printButton);
+        
+        // Register the onClick listener with the Activity that implements
+        button.setOnClickListener(printButtonListener);
     }
 }
---
+++ ice_example/python/set_env.sh
@@ -0,0 +1,2 @@
+export PYTHONPATH=/opt/Ice-3.4.2/python
+export LD_LIBRARY_PATH=/opt/Ice-3.4.2/lib
